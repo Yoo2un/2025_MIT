a0426e58790ffc1668d48c8a360aceda5ef306c0
xorhd123 <taewon493@gmail.com>
2025-09-28 14:45:47 +0900

    Added Animation
    Added animation in Player

--- MIT_project/Assets/Undead Survivor/Scripts/Player.cs
+++ MIT_project/Assets/Undead Survivor/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -9,18 +10,18 @@ public class Player : MonoBehaviour
     public float speed;
 
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter= GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
         //GetComponent<T>: 오브젝트에서 컴포넌트를 가져오는 함수
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.y = Input.GetAxisRaw("Vertical");
-    }
+    
 
     private void FixedUpdate()
     {
@@ -31,11 +32,28 @@ public class Player : MonoBehaviour
         //rigid.velocity = inputVec;
 
 
-        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         //nomalized: 벡터 값의 크기가 1이 되도록 좌표가 수정된 값
         //fixedDeltaTime: 물리 프레임 하나가 소비한 시간
 
         // 3. 위치 이동
         rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+        //SetFloat 첫번째 인자: 파라미터 이름
+        //SetFloat 두번째 인자: 반영할 float값(magnitude는 벡터의 크기)
+
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
 }
